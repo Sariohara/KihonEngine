97e281eeab6ab4849a21211b8d61a5371bbd821c
Nico <spam.nicolas.viel@gmail.com>
2021-09-19 19:32:28 +0200

    skybox load optimizations

--- source/KihonEngine/GameEngine/Graphics/ModelsBuilders/ImageHelper.cs
+++ source/KihonEngine/GameEngine/Graphics/ModelsBuilders/ImageHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Media.Imaging;
@@ -7,18 +8,28 @@ namespace KihonEngine.GameEngine.Graphics.ModelsBuilders
 {
     public static class ImageHelper
     {
+        private static Dictionary<string, BitmapImage>  _bitmapImageCache = new Dictionary<string, BitmapImage>();
+        private static Dictionary<string, Bitmap> _bitmapCache = new Dictionary<string, Bitmap>();
+
         public static BitmapImage Get(string shortResourceName)
         {
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var bitmap = new BitmapImage();
-
-            var assemblyName = typeof(ImageHelper).Assembly.GetName().Name;
-            using (var stream = assembly.GetManifestResourceStream($"{assemblyName}.Content.Images.{shortResourceName}"))
+            BitmapImage bitmap = null;
+            
+            if (!_bitmapImageCache.TryGetValue(shortResourceName, out bitmap))
             {
-                bitmap.BeginInit();
-                bitmap.StreamSource = stream;
-                bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                bitmap.EndInit();
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                bitmap = new BitmapImage();
+
+                var assemblyName = typeof(ImageHelper).Assembly.GetName().Name;
+                using (var stream = assembly.GetManifestResourceStream($"{assemblyName}.Content.Images.{shortResourceName}"))
+                {
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = stream;
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                }
+
+                _bitmapImageCache.Add(shortResourceName, bitmap);
             }
 
             return bitmap;
@@ -26,21 +37,29 @@ namespace KihonEngine.GameEngine.Graphics.ModelsBuilders
 
         public static Bitmap GetBitmap(string shortResourceName)
         {
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var bitmap = new BitmapImage();
+            Bitmap bitmap = null;
 
-            var assemblyName = typeof(ImageHelper).Assembly.GetName().Name;
-            using (var stream = assembly.GetManifestResourceStream($"{assemblyName}.Content.Images.{shortResourceName}"))
+            if (!_bitmapCache.TryGetValue(shortResourceName, out bitmap))
             {
-                return new Bitmap(stream);
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+                var assemblyName = typeof(ImageHelper).Assembly.GetName().Name;
+                using (var stream = assembly.GetManifestResourceStream($"{assemblyName}.Content.Images.{shortResourceName}"))
+                {
+                    bitmap = new Bitmap(stream);
+                }
+
+                _bitmapCache.Add(shortResourceName, bitmap);
             }
+
+            return bitmap;
         }
 
         public static BitmapImage ToBitmapImage(Bitmap bitmap)
         {
             using (var memory = new MemoryStream())
             {
-                bitmap.Save(memory, ImageFormat.Png);
+                bitmap.Save(memory, ImageFormat.Bmp);
                 memory.Position = 0;
 
                 var bitmapImage = new BitmapImage();
@@ -62,6 +81,20 @@ namespace KihonEngine.GameEngine.Graphics.ModelsBuilders
         /// <param name="row">row in skybox image</param>
         /// <returns></returns>
         public static BitmapImage GetSkyboxPart(string shortResourceName, SkyboxFace face)
+        {
+            BitmapImage bitmap = null;
+
+            var key = $"{shortResourceName}|{face}";
+            if (!_bitmapImageCache.TryGetValue(key, out bitmap))
+            {
+                bitmap = BuildSkyboxPart(shortResourceName, face);
+                _bitmapImageCache.Add(key, bitmap);
+            }
+
+            return bitmap;
+        }
+        
+        private static BitmapImage BuildSkyboxPart(string shortResourceName, SkyboxFace face)
         {
             var imageSource = GetBitmap(shortResourceName);
             var sizeX = imageSource.Size.Width / 4;
