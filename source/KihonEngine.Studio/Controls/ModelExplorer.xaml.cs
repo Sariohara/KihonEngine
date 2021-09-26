a5819e16b35693afcf5fa28e1202ed23c726a130
Nico <spam.nicolas.viel@gmail.com>
2021-09-26 17:50:26 +0200

    better synchronization for model Explorer and State Viewer

--- source/KihonEngine.Studio/Controls/ModelExplorer.xaml.cs
+++ source/KihonEngine.Studio/Controls/ModelExplorer.xaml.cs
@@ -2,8 +2,10 @@
 using KihonEngine.GameEngine.Graphics.ModelsBuilders;
 using KihonEngine.GameEngine.State;
 using KihonEngine.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -14,27 +16,70 @@ namespace KihonEngine.Studio.Controls
     /// </summary>
     public partial class ModelExplorer : UserControl, ISynchronizedIO
     {
+        private ILogService LogService
+            => Container.Get<ILogService>();
         private IGameEngineState State
             => Container.Get<IGameEngineState>();
         private IGameEngineController GameEngineController
             => Container.Get<IGameEngineController>();
 
+        private Timer timer;
+        private int timerIntervalInMilliseconds = 500;
+        private bool _processing;
+
+        private IGameEngineState _lastState;
+        private bool _stateUpdated;
+
         public ModelExplorer()
         {
             InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = timerIntervalInMilliseconds;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
         }
 
-        private bool _synchronizing;
         public void Synchronize(IGameEngineState state)
         {
-            _synchronizing = true;
+            // Defered due to high refresh rate
+            _lastState = state;
+            _stateUpdated = true;
+        }
 
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!_processing)
+            {
+                try
+                {
+                    _processing = true;
+
+                    if (_lastState != null && _stateUpdated)
+                    {
+                        Dispatcher.Invoke(() => SynchronizeInternal(_lastState));
+                        _stateUpdated = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogService.Log($"ERROR:{nameof(StateProperties)}:{ex.Message}");
+                }
+                finally
+                {
+                    _processing = false;
+                }
+            }
+        }
+
+        private void SynchronizeInternal(IGameEngineState state)
+        {
             lvModels.IsEnabled = state.EngineMode == EngineMode.EditorMode;
 
             if (lvModels.IsEnabled)
             {
                 var list = new List<ModelViewModel>();
-                var level = state.Graphics.Level.ToArray();
+                var level = state.Graphics.Level;
                 var index = 0;
                 foreach (var layeredModel in level)
                 {
@@ -47,14 +92,20 @@ namespace KihonEngine.Studio.Controls
                     list.Add(viewModel);
                 }
 
+                var lvSelectedIndex = lvModels.SelectedIndex;
+                if (state.Editor.ActionSelect.SelectedModel != null)
+                {
+                    lvSelectedIndex = level.IndexOf(state.Editor.ActionSelect.SelectedModel);
+                }
+
                 lvModels.ItemsSource = list;
+                lvModels.SelectedIndex = lvSelectedIndex;
+                lvModels.ScrollIntoView(list[lvSelectedIndex]);
             }
             else
             {
                 lvModels.ItemsSource = null;
             }
-
-            _synchronizing = false;
         }
 
         public class ModelViewModel
@@ -67,7 +118,7 @@ namespace KihonEngine.Studio.Controls
 
         private void lvModels_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (lvModels.SelectedItem != null && !_synchronizing)
+            if (lvModels.SelectedItem != null && !_processing)
             {
                 var viewModel = lvModels.SelectedItem as ModelViewModel;
                 State.Editor.ActionSelect.SelectedModel = viewModel.Model;
