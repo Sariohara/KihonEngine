0e87b8ff2b1a2dee74187aadb4601da5d25c3122
Nico <spam.nicolas.viel@gmail.com>
2021-10-15 20:11:54 +0200

    adapt Kihonengine.SampleGame to texture support

--- source/KihonEngine.SampleGame/MainWindow.xaml.cs
+++ source/KihonEngine.SampleGame/MainWindow.xaml.cs
@@ -12,6 +12,9 @@
             // Configure game
             StandardStartups.BuildStandaloneFullScreenGame();
 
+            // Register additional textures and skyboxes
+            Engine.RegisterContentFromAssembly(typeof(SampleMaps.E1M1MapBuilder));
+
             // When click a Start button
             btnStart1.Click += (sender, e) => Engine.Play<SampleMaps.E1M1MapBuilder>();
             btnStart2.Click += (sender, e) => Engine.Play<SampleMaps.Q3DM1MapBuilder>();
