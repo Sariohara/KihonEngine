e835e781279b3622b2f560854d693fd0d4614f7e
Nico <spam.nicolas.viel@gmail.com>
2021-10-17 00:05:26 +0200

    clean code

--- source/KihonEngine.Studio/App.xaml.cs
+++ source/KihonEngine.Studio/App.xaml.cs
@@ -26,7 +26,7 @@ namespace KihonEngine.Studio
         {
             DisplaySplashScreen();
 
-            // Can not move initialization sequence in a background thread because
+            // Can not move initialization sequence in a background thread. Because
             // 3D WPF objects have to be displayed in the main UI thread. So, they
             // have to born and live in the main UI thread.
 
